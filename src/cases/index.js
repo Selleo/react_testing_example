f7a64afb4178a8fc41a34f7fb85e30ea0cda5253
michalfirszt <michalfiszu@gmail.com>
2021-11-05 16:31:19 +0100

    websocket connection example

--- src/cases/index.js
+++ src/cases/index.js
@@ -1,4 +1,5 @@
 import { ExampleReachParamsMock } from "./mock_external_package_with_parameter/ExampleReachParamsMock";
+import ExampleWebsocketConnectionMock from "./mock_websocket_connection/ExampleWebsocketConnectionMock"
 
 const examples = [
   {
@@ -7,6 +8,12 @@ const examples = [
     to: "/reachParams/123/posts/456",
     linkText: "ExampleReachParamsMock",
   },
+  {
+    Component: ExampleWebsocketConnectionMock,
+    path: "/websocketConnection",
+    to: "/websocketConnection",
+    linkText: "ExampleWebsocketConnectionMock",
+  }
 ];
 
 export default examples
