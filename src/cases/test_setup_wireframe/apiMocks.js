c9fb573b03e11177a326466c4850164e5eb25783
Arkadiusz Machalica <a.machalica@selleo.com>
2022-12-08 15:35:13 +0100

    Add example mocking

--- src/cases/test_setup_wireframe/apiMocks.js
+++ src/cases/test_setup_wireframe/apiMocks.js
@@ -1,12 +1,27 @@
-// placeholder
-// you can learn more about api mocking here ...
+import {
+  getAuthMethodsMock,
+  getCountriesResponse,
+  getLanguagesResponse,
+  getCompanyBranding,
+} from "./mocks";
 
-export const mockApiAppSetup = ({mock, overrides}) => {
+export const mockApiAppSetup = ({ mock, overrides }) => {
+  const {
+    authMethods = [200, getAuthMethodsMock()],
+    countries = [200, getCountriesResponse()],
+    languages = [200, getLanguagesResponse()],
+    branding = [200, getCompanyBranding()],
+  } = overrides;
+  mock.onGet("/api/authentication_methods").reply(authMethods);
+
+  mock.onGet(`/api/countries`).reply(countries);
+  mock.onGet(`/api/languages`).reply(languages);
+  mock.onGet(`/api/branding`).reply(branding);
+};
+
+export const mockApiFeeds = ({ mock, overrides }) => {
   // mocking behavior
-}
-export const mockApiFeeds = ({mock, overrides}) => {
+};
+export const mockApiCurrentUser = ({ mock, overrides }) => {
   // mocking behavior
-}
-export const mockApiCurrentUser = ({mock, overrides}) => {
-  // mocking behavior
-}
+};
---
+++ src/cases/test_setup_wireframe/mocks.js
@@ -0,0 +1,12 @@
+export const getAuthMethodsMock = () => {
+  // fishery.js factory here
+}
+export const getCountriesResponse = () => {
+  // fishery.js factory here
+}
+export const getLanguagesResponse = () => {
+  // fishery.js factory here
+}
+export const getCompanyBranding = () => {
+  // fishery.js factory here
+}
